ae6067bca5134b4c40a2091d4f6407573ed1dda5
Antoni Nowicki <atn2122804@gmail.com>
2025-12-18 20:40:10 +0100

    build: feature/header & footer component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,38 @@
-const Header = () => {
-  return (
-    <div className="py-4 bg-white dark:bg-slate-800 top-0 sticky z-10 shadow-lg font-karla row">
-      <div style={{ fontSize: "30px", fontWeight: "bold" }}>Shopify</div>
-    </div>
-  );
-};
+import { ShoppingCart, Search, User } from "lucide-react";
+import {Link} from "react-router-dom";
+import styles from './header.module.css';
 
-export default Header;
+export default function Header() {
+  return (
+    <header>
+      <div className="d-flex mx-auto justify-content-around align-items-center" style={{background:"rgb(6 91 217 / 23%)", padding:"8px"}}>
+        {/* Logo */}
+        <Link to="/" className={styles.title} >shopify</Link>
+
+        {/* Navigation */}
+        <nav style={{fontSize:"1.2em"}}>
+          <Link to="/AllProducts" className={styles.products}>Products</Link>
+          <Link to="/AllCategories" className={styles.categories} >Categories</Link>
+        </nav>
+
+        {/* Search */}
+        <div >
+          <Search/>
+          <input type="text" placeholder="Search products" className={styles["search-input"]} />
+        </div>
+
+        {/* Actions */}
+        <div>
+          <button className="relative">
+            <ShoppingCart />
+            <span className={styles.number}>2</span>
+          </button>
+
+          <button>
+            <User />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 
 const Home = () => {
-    return (
-        <div className="main home">
-            <div>Home Page</div>
-        </div>
-    )
-}
+  return (
+    <div className="main home">
+      <div>Home Page</div>
+    </div>
+  );
+};
 
 export default Home;
