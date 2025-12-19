9d624181167e6ae01acbfe0d796ce911eafa3e65
Antoni Nowicki <atn2122804@gmail.com>
2025-12-19 04:51:34 +0100

    Build: header & footer component but not implement functionality because redux.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,67 @@
 import { ShoppingCart, Search, User } from "lucide-react";
-import {Link} from "react-router-dom";
-import styles from './header.module.css';
+import { Link } from "react-router-dom";
+import styles from "./header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [searchKey, setSearchKey] = useState<string>("");
+  const searchProducts = (e: string) => {
+    setSearchKey(e);
+  };
+  const [isLoggiedIn, setIsLoggedIn] = useState<boolean>(false);
+  const token = localStorage.getItem("auth");
+  if (token) setIsLoggedIn(true);
   return (
     <header>
-      <div className="d-flex mx-auto justify-content-around align-items-center" style={{background:"rgb(6 91 217 / 23%)", padding:"8px"}}>
+      <div
+        className="d-flex mx-auto justify-content-around align-items-center"
+        style={{ background: "rgb(6 91 217 / 23%)", padding: "8px" }}
+      >
         {/* Logo */}
-        <Link to="/" className={styles.title} >shopify</Link>
+        <Link to="/" className={styles.title}>
+          shopify
+        </Link>
 
         {/* Navigation */}
-        <nav style={{fontSize:"1.2em"}}>
-          <Link to="/AllProducts" className={styles.products}>Products</Link>
-          <Link to="/AllCategories" className={styles.categories} >Categories</Link>
+        <nav style={{ fontSize: "1.2em" }}>
+          <Link to="/AllProducts" className={styles.products}>
+            Products
+          </Link>
+          <Link to="/AllCategories" className={styles.categories}>
+            Categories
+          </Link>
         </nav>
 
         {/* Search */}
-        <div >
-          <Search/>
-          <input type="text" placeholder="Search products" className={styles["search-input"]} />
+        <div>
+          <Search />
+          <input
+            type="text"
+            placeholder="Search products"
+            className={styles["search-input"]}
+            onChange={(e) => {
+              searchProducts(e.target.value);
+            }}
+            value={searchKey}
+          />
         </div>
 
         {/* Actions */}
-        <div>
+        <div style={{ display: "inline-flex" }}>
           <button className="relative">
             <ShoppingCart />
             <span className={styles.number}>2</span>
           </button>
 
-          <button>
-            <User />
-          </button>
+          <div>
+            {isLoggiedIn ? (
+              <img src="/public/kyo.jfif" className="avatar"></img>
+            ) : (
+              <button>
+                <User />
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </header>
